74cf2bb86520fb641a83606e3c059e2859e1af94
muazzia17 <muazzia17@gmail.com>
2026-04-19 15:28:39 +0500

    Refactor User class to adhere to Single Responsibility Principle by separating user data and authentication into distinct classes.

--- src/concepts/solid/single-responsibility/index.ts
+++ src/concepts/solid/single-responsibility/index.ts
@@ -21,4 +21,39 @@ class User {
 
 (() => {
   const muaz = new User("Muaz", "123123");
+  console.log(muaz.authenticate("Muaz", "123123"));
 })();
+
+// The above example shows the user class is handling 2 things
+// First is the user data and the second one is authentication
+
+class UserData {
+  userName: string;
+  password: string;
+
+  constructor(userName: string, password: string) {
+    this.userName = userName;
+    this.password = password;
+  }
+}
+
+class UserAuthentication {
+  private _user: UserData;
+
+  constructor(user: UserData) {
+    this._user = user;
+  }
+
+  authenticate(userName: string, password: string) {
+    if (this._user.userName === userName && this._user.password === password) {
+      return true;
+    } else {
+      return "Invalid";
+    }
+  }
+}
+
+const newMuaz = new UserData("Muaz", "123123");
+const auth = new UserAuthentication(newMuaz);
+// const auth = new UserAuthentication(newMuaz);
+console.log(auth.authenticate("Muaz", "1231231"));
